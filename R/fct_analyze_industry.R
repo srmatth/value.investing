bd7b8c269b5a682165fae5c3a9c77c98c414020a
srmatth <srmatthews98@gmail.com>
2020-12-15 08:57:01 -0700

    First version of the new industry analysis function

--- R/fct_analyze_industry.R
+++ R/fct_analyze_industry.R
@@ -94,9 +94,9 @@ analyze_industry <- function(industry) {
   
   random_forest_growth(.data = price_dat, industry = industry)
   
-  #gradient_boosted_binary(.data = price_dat, industry = industry)
+  gradient_boosted_binary(.data = price_dat, industry = industry)
   
-  #gradient_boosted_growth(.data = price_dat, industry = industry)
+  gradient_boosted_growth(.data = price_dat, industry = industry)
 }
 
 #### download_prices ----
@@ -183,17 +183,17 @@ random_forest_binary <- function(.data, industry) {
   h2o::h2o.init(enable_assertions = FALSE)
   
   grid <- list(
-    ntrees = c(50),
+    ntrees = c(50, 100),
     max_depth = c(3, 7, 10, 15, 20),
     min_rows = c(1),
-    mtries = c(7),
-    min_split_improvement = c(.0001),
+    mtries = c(4, 7),
+    min_split_improvement = c(.0001, .001),
     sample_rate = c(.632),
-    nbins = c(4),
-    binomial_double_trees = c(TRUE),
+    nbins = c(4, 20),
     col_sample_rate_per_tree = c(.8),
-    balance_classes = c(TRUE),
-    seed = 16
+    seed = 16,
+    binomial_double_trees = TRUE,
+    balance_classes = TRUE
   ) %>%
     expand.grid(stringsAsFactors = FALSE)
   
@@ -376,7 +376,7 @@ random_forest_binary <- function(.data, industry) {
   readr::write_csv(all_preds, paste0(path, "/rf_binary_preds.csv"))
 }
 
-#### randm_forest_growth ----
+#### random_forest_growth ----
 
 random_forest_growth <- function(.data, industry) {
   
@@ -415,16 +415,14 @@ random_forest_growth <- function(.data, industry) {
   h2o::h2o.init(enable_assertions = FALSE)
   
   grid <- list(
-    ntrees = c(50),
+    ntrees = c(50, 100),
     max_depth = c(3, 7, 10, 15, 20),
     min_rows = c(1),
     mtries = c(4, 7),
-    min_split_improvement = c(.0001),
+    min_split_improvement = c(.0001, .001),
     sample_rate = c(.632),
-    nbins = c(4),
-    binomial_double_trees = c(TRUE),
+    nbins = c(4, 20),
     col_sample_rate_per_tree = c(.8),
-    balance_classes = c(TRUE),
     seed = 16
   ) %>%
     expand.grid(stringsAsFactors = FALSE)
@@ -456,9 +454,7 @@ random_forest_growth <- function(.data, industry) {
         min_split_improvement = grid_sub$min_split_improvement,
         sample_rate = grid_sub$sample_rate,
         nbins = grid_sub$nbins,
-        binomial_double_trees = grid_sub$binomial_double_trees,
         seed = grid_sub$seed,
-        balance_classes = grid_sub$balance_classes,
         col_sample_rate_per_tree = grid_sub$col_sample_rate_per_tree
       )
       time <- tictoc::toc()
@@ -516,7 +512,7 @@ random_forest_growth <- function(.data, industry) {
   final_mod <- h2o::h2o.randomForest(
     y = "three_year_growth",
     training_frame = mod_dat_h,
-    model_id = "final_forest_mod_growth",
+    model_id = "final_rf_growth_mod",
     nfolds = 5,
     ntrees = best_model$ntrees,
     max_depth = best_model$max_depth,
@@ -546,15 +542,6 @@ random_forest_growth <- function(.data, industry) {
     ) %>%
     cbind(as.data.frame(newdata))
   
-  preds_98 <- predict(final_mod_98, newdata) %>%
-    as.data.frame() %>%
-    dplyr::mutate(
-      predict = dplyr::if_else(yes > thresh_98, "yes", "no"),
-      mod_num = best_model_98$mod_num,
-      metric = "98% specificity"
-    ) %>%
-    cbind(as.data.frame(newdata))
-  
   h2o::h2o.saveModel(final_mod, path, force = TRUE)
   
   readr::write_csv(results, paste0(path, "/rf_growth_tuning_results.csv"))
@@ -565,11 +552,414 @@ random_forest_growth <- function(.data, industry) {
 #### gradient_boosted_binary ----
 
 gradient_boosted_binary <- function(industry) {
+  path <- stringr::str_c("data/industries/", industry)
   
+  response <- .data %>%
+    dplyr::select(ticker, quarter, avg_price) %>%
+    dplyr::mutate(quarter = quarter - 3, avg_future_price = avg_price) %>%
+    dplyr::select(-avg_price)
+  
+  usethis::ui_info("Compiling model Data for {industry} GB binary models")
+  mod_dat <- .data %>%
+    dplyr::left_join(response, by = c("ticker", "quarter")) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!is.na(avg_future_price)) %>%
+    dplyr::mutate(
+      three_year_growth = avg_future_price / avg_price - 1,
+      over_40_growth = as.factor(dplyr::if_else(three_year_growth > 0.4, "yes", "no"))
+    ) %>%
+    dplyr::select(
+      -ticker, 
+      -three_year_growth, 
+      -avg_future_price, 
+      -quarter, 
+      -year,
+      -yearly_amount_per_share
+    ) %>%
+    # center and scale some predictors
+    dplyr::mutate(
+      avg_vol = (avg_vol - mean(avg_vol)) / sd(avg_vol),
+      avg_price = (avg_price - mean(avg_price)) / sd(avg_price),
+      avg_pct_change = (avg_pct_change - mean(avg_pct_change)) / sd(avg_pct_change),
+      sd_pct_change = (sd_pct_change - mean(sd_pct_change)) / sd(sd_pct_change),
+      sd_price = (sd_price - mean(sd_price)) / sd(sd_price),
+      avg_range = (avg_range - mean(avg_range)) / sd(avg_range),
+      avg_vol = (avg_vol - mean(avg_vol)) / sd(avg_vol)
+    )
+  
+  h2o::h2o.init(enable_assertions = FALSE)
+  
+  grid <- list(
+    ntrees = c(100, 300),
+    max_depth = c(1, 3, 7, 10, 15),
+    min_rows = c(1),
+    distribution = c("bernoulli"),
+    learn_rate = c(0.05, 0.01, 0.001),
+    learn_rate_annealing = c(0.99, 0.9, 0.5),
+    min_split_improvement = c(.0001),
+    sample_rate = c(.632),
+    nbins = c(4),
+    col_sample_rate_per_tree = c(.8),
+    histogram_type = c("RoundRobin"),
+    seed = 16
+  ) %>%
+    expand.grid(stringsAsFactors = FALSE)
+  
+  mod_dat_h <- h2o::as.h2o(mod_dat) %>%
+    h2o::h2o.splitFrame()
+  
+  train <- mod_dat_h[[1]]
+  test <- mod_dat_h[[2]]
+  
+  # initialize the data frames where we will save the results
+  results <- data.frame(stringsAsFactors = FALSE)
+  # get the counts for each level in each frame
+  n_yes_train <- sum(train["over_40_growth"] == "yes")
+  n_no_train <- sum(train["over_40_growth"] == "no")
+  n_yes_test <- sum(test["over_40_growth"] == "yes")
+  n_no_test <- sum(test["over_40_growth"] == "no")
+  
+  for (i in 1:nrow(grid)) {
+    grid_sub <- grid %>% dplyr::slice(i)
+    
+    tryCatch({
+      usethis::ui_info("Starting model {i}")
+      tictoc::tic(stringr::str_c("model", i, sep = " "))
+      tmp_mod <- h2o::h2o.gbm(
+        y = "over_40_growth",
+        training_frame = train,
+        nfolds = 5,
+        model_id = "temp_gb_mod",
+        ntrees = grid_sub$ntrees,
+        max_depth = grid_sub$max_depth,
+        min_rows = grid_sub$min_rows,
+        distribution = grid_sub$distribution,
+        learn_rate = grid_sub$learn_rate,
+        learn_rate_annealing = grid_sub$learn_rate_annealing,
+        min_split_improvement = grid_sub$min_split_improvement,
+        sample_rate = grid_sub$sample_rate,
+        nbins = grid_sub$nbins,
+        col_sample_rate_per_tree = grid_sub$col_sample_rate_per_tree,
+        histogram_type = grid_sub$histogram_type,
+        seed = grid_sub$seed
+      )
+      time <- tictoc::toc()
+      
+      usethis::ui_info("Model {i} trained")
+      
+      perf <- h2o::h2o.performance(tmp_mod, test)
+      threshold_max_f1 <- h2o::h2o.find_threshold_by_max_metric(perf, "f1")
+      
+      threshold_98_specificity <- h2o::h2o.metric(perf, metric = "specificity") %>%
+        dplyr::filter(specificity > .98) %>%
+        dplyr::pull(threshold) %>%
+        min()
+      
+      usethis::ui_info("Model {i} tested")
+      
+      results_tmp <- data.frame(
+        mod_num = i,
+        time_to_create = time$toc - time$tic,
+        n_yes_train = n_yes_train,
+        n_no_train = n_no_train,
+        n_yes_test = n_yes_test,
+        n_no_test = n_no_test,
+        ntrees = grid_sub$ntrees,
+        ntrees = grid_sub$ntrees,
+        max_depth = grid_sub$max_depth,
+        min_rows = grid_sub$min_rows,
+        distribution = grid_sub$distribution,
+        learn_rate = grid_sub$learn_rate,
+        learn_rate_annealing = grid_sub$learn_rate_annealing,
+        min_split_improvement = grid_sub$min_split_improvement,
+        sample_rate = grid_sub$sample_rate,
+        nbins = grid_sub$nbins,
+        col_sample_rate_per_tree = grid_sub$col_sample_rate_per_tree,
+        histogram_type = grid_sub$histogram_type,
+        gini = h2o::h2o.giniCoef(tmp_mod, train = TRUE),
+        auc = h2o::h2o.auc(tmp_mod, train = TRUE),
+        threshold_max_f1 = threshold_max_f1,
+        sensitivity_max_f1 = as.numeric(h2o::h2o.sensitivity(perf, threshold_max_f1)),
+        specificity_max_f1 = as.numeric(h2o::h2o.specificity(perf, threshold_max_f1)),
+        precision_max_f1 = as.numeric(h2o::h2o.precision(perf, threshold_max_f1)),
+        accuracy_max_f1 = as.numeric(h2o::h2o.accuracy(perf, threshold_max_f1)),
+        threshold_98_specificity =  threshold_98_specificity,
+        sensitivity_98_specificity = as.numeric(h2o::h2o.sensitivity(perf, threshold_98_specificity)),
+        specificity_98_specificity = as.numeric(h2o::h2o.specificity(perf, threshold_98_specificity)),
+        precision_98_specificity = as.numeric(h2o::h2o.precision(perf, threshold_98_specificity)),
+        accuracy_98_specificity = as.numeric(h2o::h2o.accuracy(perf, threshold_98_specificity)),
+        stringsAsFactors = FALSE
+      )
+      
+      results <- rbind(results, results_tmp)
+      usethis::ui_done("Model {i} finished and data saved")
+    },
+    error = function(e) {
+      usethis::ui_oops("Model {i} failed! {e}")
+    })
+  }
+  
+  best_model_98 <- results %>%
+    dplyr::arrange(desc(sensitivity_98_specificity)) %>%
+    dplyr::slice(1)
+  best_model_f1 <- results %>%
+    dplyr::arrange(desc(specificity_max_f1)) %>%
+    dplyr::slice(1)
+  
+  mod_dat_h <- h2o::as.h2o(mod_dat)
+  
+  final_mod_f1 <- h2o::h2o.randomForest(
+    y = "over_40_growth",
+    training_frame = mod_dat_h,
+    model_id = "final_gb_prob_mod_f1",
+    nfolds = 5,
+    ntrees = best_model_f1$ntrees,
+    max_depth = best_model_f1$max_depth,
+    min_rows = best_model_f1$min_rows,
+    distribution = best_model_f1$distribution,
+    learn_rate = best_model_f1$learn_rate,
+    learn_rate_annealing = best_model_f1$learn_rate_annealing,
+    min_split_improvement = best_model_f1$min_split_improvement,
+    sample_rate = best_model_f1$sample_rate,
+    nbins = best_model_f1$nbins,
+    col_sample_rate_per_tree = best_model_f1$col_sample_rate_per_tree,
+    histogram_type = best_model_f1$histogram_type
+  )
+  
+  final_mod_98 <- h2o::h2o.randomForest(
+    y = "over_40_growth",
+    training_frame = mod_dat_h,
+    model_id = "final_gb_prob_mod_98",
+    nfolds = 5,
+    ntrees = best_model_98$ntrees,
+    max_depth = best_model_98$max_depth,
+    min_rows = best_model_98$min_rows,
+    distribution = best_model_98$distribution,
+    learn_rate = best_model_98$learn_rate,
+    learn_rate_annealing = best_model_98$learn_rate_annealing,
+    min_split_improvement = best_model_98$min_split_improvement,
+    sample_rate = best_model_98$sample_rate,
+    nbins = best_model_98$nbins,
+    col_sample_rate_per_tree = best_model_98$col_sample_rate_per_tree,
+    histogram_type = best_model_98$histogram_type
+  )
+  
+  f1_thresh <- results %>%
+    dplyr::arrange(desc(specificity_max_f1)) %>%
+    dplyr::slice(1) %>%
+    dplyr::pull(threshold_max_f1)
+  
+  thresh_98 <- results %>%
+    dplyr::arrange(desc(sensitivity_98_specificity)) %>%
+    dplyr::slice(1) %>%
+    dplyr::pull(threshold_98_specificity)
+  
+  newdata <- .data %>% 
+    dplyr::filter(year == 2020) %>%
+    h2o::as.h2o()
+  
+  preds_f1 <- predict(final_mod_f1, newdata) %>%
+    as.data.frame() %>%
+    dplyr::mutate(
+      predict = dplyr::if_else(yes > f1_thresh, "yes", "no"),
+      mod_num = best_model_f1$mod_num,
+      metric = "f1"
+    ) %>%
+    cbind(as.data.frame(newdata))
+  
+  preds_98 <- predict(final_mod_98, newdata) %>%
+    as.data.frame() %>%
+    dplyr::mutate(
+      predict = dplyr::if_else(yes > thresh_98, "yes", "no"),
+      mod_num = best_model_98$mod_num,
+      metric = "98% specificity"
+    ) %>%
+    cbind(as.data.frame(newdata))
+  
+  all_preds <- rbind(preds_f1, preds_98) %>%
+    dplyr::arrange(desc(yes))
+  
+  h2o::h2o.saveModel(final_mod_f1, path, force = TRUE)
+  h2o::h2o.saveModel(final_mod_98, path, force = TRUE)
+  
+  readr::write_csv(results, paste0(path, "/gb_prob_tuning_results.csv"))
+  readr::write_csv(all_preds, paste0(path, "/gb_prob_preds.csv"))
 }
 
 #### gradient_boosted_growth ----
 
 gradient_boosted_growth <- function(industry) {
+  path <- stringr::str_c("data/industries/", industry)
   
+  response <- .data %>%
+    dplyr::select(ticker, quarter, avg_price) %>%
+    dplyr::mutate(quarter = quarter - 3, avg_future_price = avg_price) %>%
+    dplyr::select(-avg_price)
+  
+  usethis::ui_info("Compiling model data for {industry} GB binary models")
+  mod_dat <- .data %>%
+    dplyr::left_join(response, by = c("ticker", "quarter")) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!is.na(avg_future_price)) %>%
+    dplyr::mutate(three_year_growth = avg_future_price / avg_price - 1) %>%
+    dplyr::select(
+      -ticker, 
+      -avg_future_price, 
+      -quarter, 
+      -year,
+      -yearly_amount_per_share
+    ) %>%
+    # center and scale some predictors
+    dplyr::mutate(
+      avg_vol = (avg_vol - mean(avg_vol)) / sd(avg_vol),
+      avg_price = (avg_price - mean(avg_price)) / sd(avg_price),
+      avg_pct_change = (avg_pct_change - mean(avg_pct_change)) / sd(avg_pct_change),
+      sd_pct_change = (sd_pct_change - mean(sd_pct_change)) / sd(sd_pct_change),
+      sd_price = (sd_price - mean(sd_price)) / sd(sd_price),
+      avg_range = (avg_range - mean(avg_range)) / sd(avg_range),
+      avg_vol = (avg_vol - mean(avg_vol)) / sd(avg_vol),
+      three_year_growth = log(three_year_growth + 1.1)
+    )
+  
+  h2o::h2o.init(enable_assertions = FALSE)
+  
+  grid <- list(
+    ntrees = c(100, 300),
+    max_depth = c(1, 3, 7, 10),
+    min_rows = c(1),
+    distribution = c("gaussian", "gamma"),
+    learn_rate = c(0.05, 0.01, 0.001),
+    learn_rate_annealing = c(0.99, 0.9, 0.5),
+    min_split_improvement = c(.0001),
+    sample_rate = c(.632),
+    nbins = c(4),
+    col_sample_rate_per_tree = c(.8),
+    histogram_type = c("RoundRobin"),
+    seed = 16
+  ) %>%
+    expand.grid(stringsAsFactors = FALSE)
+  
+  mod_dat_h <- h2o::as.h2o(mod_dat) %>%
+    h2o::h2o.splitFrame()
+  
+  train <- mod_dat_h[[1]]
+  test <- mod_dat_h[[2]]
+  
+  # initialize the data frames where we will save the results
+  results <- data.frame(stringsAsFactors = FALSE)
+  
+  for (i in 1:nrow(grid)) {
+    grid_sub <- grid %>% dplyr::slice(i)
+    
+    tryCatch({
+      usethis::ui_info("Starting model {i}")
+      tictoc::tic(stringr::str_c("model", i, sep = " "))
+      tmp_mod <- h2o::h2o.gbm(
+        y = "three_year_growth",
+        training_frame = train,
+        nfolds = 5,
+        model_id = "temp_gb_mod",
+        ntrees = grid_sub$ntrees,
+        max_depth = grid_sub$max_depth,
+        min_rows = grid_sub$min_rows,
+        distribution = grid_sub$distribution,
+        learn_rate = grid_sub$learn_rate,
+        learn_rate_annealing = grid_sub$learn_rate_annealing,
+        min_split_improvement = grid_sub$min_split_improvement,
+        sample_rate = grid_sub$sample_rate,
+        nbins = grid_sub$nbins,
+        col_sample_rate_per_tree = grid_sub$col_sample_rate_per_tree,
+        histogram_type = grid_sub$histogram_type,
+        seed = grid_sub$seed
+      )
+      time <- tictoc::toc()
+      
+      usethis::ui_info("Model {i} trained")
+      
+      perf <- h2o::h2o.performance(tmp_mod, test)
+      
+      test_preds <- predict(tmp_mod, test) %>%
+        as.data.frame() %>%
+        dplyr::mutate(
+          predict_unscaled = exp(predict) - 1.1,
+          actual_unscaled = test %>%
+            as.data.frame() %>%
+            dplyr::mutate(three_year_growth = exp(three_year_growth) - 1.1) %>%
+            dplyr::pull(three_year_growth)
+        )
+      usethis::ui_info("Model {i} tested")
+      
+      results_tmp <- data.frame(
+        mod_num = i,
+        time_to_create = time$toc - time$tic,
+        ntrees = grid_sub$ntrees,
+        max_depth = grid_sub$max_depth,
+        min_rows = grid_sub$min_rows,
+        distribution = grid_sub$distribution,
+        learn_rate = grid_sub$learn_rate,
+        learn_rate_annealing = grid_sub$learn_rate_annealing,
+        min_split_improvement = grid_sub$min_split_improvement,
+        sample_rate = grid_sub$sample_rate,
+        nbins = grid_sub$nbins,
+        col_sample_rate_per_tree = grid_sub$col_sample_rate_per_tree,
+        histogram_type = grid_sub$histogram_type,
+        r_2 = h2o::h2o.r2(tmp_mod),
+        r_2_test = h2o::h2o.r2(perf),
+        mae_test = mean(abs(test_preds$actual_unscaled - test_preds$predict_unscaled)),
+        mse_test = mean((test_preds$actual_unscaled - test_preds$predict_unscaled)^2),
+        stringsAsFactors = FALSE
+      )
+      
+      results <- rbind(results, results_tmp)
+      usethis::ui_done("Model {i} finished and data saved")
+    },
+    error = function(e) {
+      usethis::ui_oops("Model {i} failed! {e}")
+    })
+  }
+  
+  best_model <- results %>%
+    dplyr::arrange(mse_test) %>%
+    dplyr::slice(1)
+  
+  mod_dat_h <- h2o::as.h2o(mod_dat)
+  
+  final_mod <- h2o::h2o.gbm(
+    y = "three_year_growth",
+    training_frame = train,
+    nfolds = 5,
+    model_id = "final_gb_growth_mod",
+    ntrees = best_model$ntrees,
+    max_depth = best_model$max_depth,
+    min_rows = best_model$min_rows,
+    distribution = best_model$distribution,
+    learn_rate = best_model$learn_rate,
+    learn_rate_annealing = best_model$learn_rate_annealing,
+    min_split_improvement = best_model$min_split_improvement,
+    sample_rate = best_model$sample_rate,
+    nbins = best_model$nbins,
+    col_sample_rate_per_tree = best_model$col_sample_rate_per_tree,
+    histogram_type = best_model$histogram_type
+  )
+  
+  newdata <- .data %>% 
+    dplyr::filter(year == 2020) %>%
+    h2o::as.h2o()
+  
+  explain_growth <- h2o::h2o.predict_contributions(final_mod, newdata) %>%
+    as.data.frame()
+  
+  preds <- predict(final_mod, newdata) %>%
+    as.data.frame() %>%
+    dplyr::mutate(
+      mod_num = best_model$mod_num,
+      predict = exp(predict) - 1.1
+    ) %>%
+    cbind(as.data.frame(newdata))
+  
+  h2o::h2o.saveModel(final_mod, path, force = TRUE)
+  
+  readr::write_csv(results, paste0(path, "/gb_growth_tuning_results.csv"))
+  readr::write_csv(preds, paste0(path, "/gb_growth_preds.csv"))
+  readr::write_csv(explain_growth, paste0(path, "/gb_growth_explained.csv"))
 }
